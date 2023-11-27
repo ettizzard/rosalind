a7bcb115bd28c27f0e75fed9d922220d4ad1c2d8
ettizzard <149412561+ettizzard@users.noreply.github.com>
2023-11-27 22:25:18 +0000

    added python script

---
+++ RNA_Transcribing_DNA_into_RNA/RNA.py
@@ -0,0 +1,9 @@
+#!usr/bin/python3
+
+with open("/workspaces/rosalind/RNA_Transcribing_DNA_into_RNA/rosalind_dna.txt", "r"rosalind_rna.txt", "r") as dataset:
+    
+    DNAseq = dataset.read()
+    
+    RNAseq = DNAseq.replace("T", "U")
+            
+    print(RNAseq)
