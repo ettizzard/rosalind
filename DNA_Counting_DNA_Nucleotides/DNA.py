9f1f806740bc70099de194e90ebd83e709775c3b
ettizzard <149412561+ettizzard@users.noreply.github.com>
2023-11-27 22:19:05 +0000

    fixed filepath

--- DNA_Counting_DNA_Nucleotides/DNA.py
+++ DNA_Counting_DNA_Nucleotides/DNA.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-with open("rosalind_dna.txt", "r") as dataset:
+with open("/workspaces/rosalind/DNA_Counting_DNA_Nucleotides/rosalind_dna.txt", "r") as dataset:
     seq = dataset.read()
     print(seq.count("A"), seq.count("C"), seq.count("G"), seq.count("T"))
